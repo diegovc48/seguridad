bcad6ada265f39313cc29d55fecd5d9ef3ccee45
diegovc48 <diegovillamizar@gmail.com>
2025-08-29 14:32:34 -0500

    Corrección en app.py con validación y mejora de guardado2

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def pagina_blanca():
     return render_template_string("<html><body></body></html>")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto que Render asigna
+    app.run(host="0.0.0.0", port=port, debug=True)
